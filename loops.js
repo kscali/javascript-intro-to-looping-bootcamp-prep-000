11a0a07f63aef2a65255375217657023ea98d78c
kosatun@aol.com <kosatun@aol.com>
2018-07-02 04:52:42 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,18 @@
 function forLoop(array) {
   for (var i = 0; i < 25; i++) {
+<<<<<<< HEAD
    array.push(`I am ${i} strange loop.`);
   }
   return array; 
+=======
+    return "I am ${i} strange loops";
+  }
+>>>>>>> 59e37098234fcce63b8aacc724aa18c618f5228a
 }
 
 function whileLoop(n) {
   while (n > 0) {
+<<<<<<< HEAD
     console.log(n);
     --n;
   }
@@ -19,4 +25,14 @@ function doWhileLoop(array) {
     array.pop();
   } while(array.length > 0 || array.length === " ");
   return array;
+=======
+    console.log(--n);
+  }
+}
+
+function doWhileLoop(array) {
+  do {
+    return --array;
+  } while(array.length > 0);
+>>>>>>> 59e37098234fcce63b8aacc724aa18c618f5228a
 }
